9f53a34b2f23eb22ee6e8ddc1136570f98ef4a85
Pillutla <H309233@intellig.local>
2018-09-10 22:58:22 -0400

    adding content

--- dev/docker/docker-compose-dev-picking.bat
+++ dev/docker/docker-compose-dev-picking.bat
@@ -4,7 +4,7 @@ rem FOR /f "tokens=*" %i IN ('docker ps -a -q') DO docker rm %i
 docker stop postgres
 docker stop picking
 docker rm postgres
-docker rm postgres
+docker rm picking
 docker-compose -f docker-compose-dev-postgres.yml up --build -d
 docker-compose -f docker-compose-dev-picking.yml up --build -d
 docker logs -f picking
