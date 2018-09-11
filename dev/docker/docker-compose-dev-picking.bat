e08d9aed7b553497e07f38061f93c9d3e9ec760f
Pillutla <H309233@intellig.local>
2018-09-10 22:53:09 -0400

    adding content

---
+++ dev/docker/docker-compose-dev-picking.bat
@@ -0,0 +1,10 @@
+rem cd C:\Users\krishna.pillutla\eclipse-workspace\picking
+rem FOR /f "tokens=*" %i IN ('docker ps -a -q') DO docker stop %i
+rem FOR /f "tokens=*" %i IN ('docker ps -a -q') DO docker rm %i
+docker stop postgres
+docker stop picking
+docker rm postgres
+docker rm postgres
+docker-compose -f docker-compose-dev-postgres.yml up --build -d
+docker-compose -f docker-compose-dev-picking.yml up --build -d
+docker logs -f picking
