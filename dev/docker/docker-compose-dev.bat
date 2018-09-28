91ff780b7f9605b620dafa3d3e9eaf644356c794
Krishna Pillutla <knpillutla@gmail.com>
2018-09-28 14:36:46 -0400

    Adding Content

--- dev/docker/docker-compose-dev.bat
+++ dev/docker/docker-compose-dev.bat
@@ -1,8 +1,10 @@
 rem cd C:\Users\krishna.pillutla\eclipse-workspace\picking
 rem FOR /f "tokens=*" %i IN ('docker ps -a -q') DO docker stop %i
 rem FOR /f "tokens=*" %i IN ('docker ps -a -q') DO docker rm %i
-rem docker-compose -f docker-compose-dev-elk.yml up --build -d
+docker-compose -f docker-compose-dev-elk.yml up --build -d
+docker-compose -f docker-compose-dev-postgres.yml up --build -d
 docker-compose -f docker-compose-dev-config-server.yml up --build -d
+docker-compose -f docker-compose-dev-all-services.yml up --build -d
 rem docker-compose -f docker-compose-dev-kafka.yml up --build -d
 rem docker-compose -f docker-compose-dev-postgres.yml up --build -d
 rem docker-compose -f docker-compose-dev-picking.yml up --build -d
