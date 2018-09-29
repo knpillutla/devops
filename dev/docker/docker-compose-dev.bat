e64dfd19fc52cf33094327727ddc0d8e03aa35a6
Krishna Pillutla <knpillutla@gmail.com>
2018-09-29 18:53:08 -0400

    Adding Content

--- dev/docker/docker-compose-dev.bat
+++ dev/docker/docker-compose-dev.bat
@@ -1,9 +1,31 @@
 rem cd C:\Users\krishna.pillutla\eclipse-workspace\picking
 rem FOR /f "tokens=*" %i IN ('docker ps -a -q') DO docker stop %i
 rem FOR /f "tokens=*" %i IN ('docker ps -a -q') DO docker rm %i
+rem call docker stop kafka
+rem call docker stop zookeeper
+rem call docker stop config-server
+call docker stop postgres
+call docker stop picking
+call docker stop packing
+call docker stop shipping
+call docker stop inventory
+call docker stop orderplanner
+call docker stop customer-order
+rem call docker rm kafka
+rem call docker rm zookeeper
+rem call docker rm config-server
+call docker rm postgres
+call docker rm picking
+call docker rm packing
+call docker rm shipping
+call docker rm inventory
+call docker rm orderplanner
+call docker rm customer-order
+
 docker-compose -f docker-compose-dev-elk.yml up --build -d
 docker-compose -f docker-compose-dev-postgres.yml up --build -d
-docker-compose -f docker-compose-dev-config-server.yml up --build -d
+rem docker-compose -f docker-compose-dev-kafka.yml up --build -d
+rem docker-compose -f docker-compose-dev-config-server.yml up --build -d
 docker-compose -f docker-compose-dev-all-services.yml up --build -d
 rem docker-compose -f docker-compose-dev-kafka.yml up --build -d
 rem docker-compose -f docker-compose-dev-postgres.yml up --build -d
