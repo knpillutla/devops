301d0c287658459c7161d8c41e5af8d1ab44f360
Krishna Pillutla <knpillutla@gmail.com>
2018-12-05 21:30:21 -0500

    dfdf

--- gcp/kubernetes/scripts/initiate-deployment.bat
+++ gcp/kubernetes/scripts/initiate-deployment.bat
@@ -1,5 +1,6 @@
 rem call kubectl apply -f pod-global-mem-limit.yaml
 rem create deployments/services
 call kubectl apply -f timescaledb-service-kubernetes.yaml
+TIMEOUT 20
 call kubectl apply -f config-service-kubernetes.yaml
 kubectl get pods
